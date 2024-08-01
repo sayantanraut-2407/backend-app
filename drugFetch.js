16f1728ff6c0b857a41bb56a9d86b9f6cac2e9be
Sayantan Raut <rautsayantan2407@gmail.com>
2024-08-01 11:43:35 -0700

    Added endpoint to fetch drugs data

--- drugFetch.js
+++ drugFetch.js
@@ -25,10 +25,116 @@ const getDrugs = async () => {
       });
     } catch (error_1) {
       console.error(error_1);
-      throw new Error("Internal server error");
+      throw new Error("Error occurred while serving this request: " + error_1);
     }
   };
 
+  // getting complete drug data along with corresponding activity data
+  const getDrugData = async() => {
+    try {
+      return await new Promise(function (resolve, reject) {
+        pool.query("select * from mapping_drug d left join mapping_activity a on d.cid = a.cid_id where\
+        a.activity_type is not null and a.activity_value is not null order by d.cid;", (err, results) => {
+          if (err) {
+            reject(err);
+          }
+          if (results && results.rows) {
+            resolve(results.rows);
+          } else {
+            reject(new Error("No results found!"));
+          }
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      throw new Error("Error occurred while serving this request: " + error);
+    }
+  }
+
+  // getting viability and mapping data
+  const getViabilityAndMappingData = async() => {
+    try {
+      return await new Promise(function (resolve, reject) {
+        pool.query("select * from mapping_viability v left join mapping_sample s on v.sample_id = s.id\
+        left join mapping_map m on v.id = m.viability_id\
+        left join mapping_patient p on s.patient_id = p.id\
+        left join mapping_diagnosis d on p.diagnosis_id = d.id\
+        order by v.creation_date desc;", (err, results) => {
+          if (err) {
+            reject(err);
+          }
+          if (results && results.rows) {
+            const formattedData = results.rows.map((row, index) => {
+              const well_viabilities = [];
+              for (let i = 1; i <= 96; i++) {
+                well_viabilities.push({ [`well_${i}`]: row[`well_${i}`] });
+              }
+
+              const well_errors = [];
+              for (let i = 1; i <= 96; i++) {
+                well_errors.push({ [`well_${i}`]: row[`well_${i}_error`] })
+              }
+
+              const well_drug_combos = [];
+              for (let i = 1; i<= 96; i++) {
+                well_drug_combos.push({
+                  [`well_${i}_d1c`]: row[`well_${i}_drug_1_conc`],
+                  [`well_${i}_d2c`]: row[`well_${i}_drug_2_conc`],
+                  [`well_${i}_d3c`]: row[`well_${i}_drug_3_conc`],
+                  [`well_${i}_d4c`]: row[`well_${i}_drug_4_conc`],
+                  [`well_${i}_d1id`]: row[`well_${i}_drug_1_id`],
+                  [`well_${i}_d2id`]: row[`well_${i}_drug_2_id`],
+                  [`well_${i}_d3id`]: row[`well_${i}_drug_3_id`],
+                  [`well_${i}_d4id`]: row[`well_${i}_drug_4_id`]
+                })
+              }
+        
+              return {
+                local_record_id: index + 1,
+                viability_id: row.viability_id,
+                viability_name: row.viability_name,
+                viability_creation_date: row.creation_date,
+                sample_id: row.sample_id,
+                collection_date: row.collection_date,
+                experiment_date: row.experiment_date,
+                experiment_type: row.experiment_type,
+                tissue_type: row.tissue_type,
+                tissue_of_origin: row.tissue_of_origin,
+                procurement_type: row.procurement_type,
+                preservation: row.preservation,
+                location_information: row.location_information,
+                specimen_lab_name: row.specimen_lab_name,
+                collection_id: row.collection_id,
+                patient_id: row.patient_id,
+                screen: row.screen,
+                note: row.note,
+                map_name: row.map_name,
+                age_at_diagnosis: row.age_at_diagnosis,
+                metastatic_status: row.metastatic_status,
+                has_Y_chromosome: row.has_Y_chromosome,
+                diagnosis_id: row.diagnosis_id,
+                diagnosis: row.diagnosis,
+                icd_code: row.icd_code,
+                well_viabilities: well_viabilities,
+                well_errors: well_errors,
+                well_drug_combos: well_drug_combos
+              };
+            });
+            //console.log(JSON.stringify(formattedData));
+            resolve(formattedData);
+          } else {
+            reject(new Error("No results found!"));
+          }
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      throw new Error("Error occurred while serving this request: " + error);
+    }
+  }
+
   module.exports = {
-    getDrugs
+    getDrugs,
+    getDrugData,
+    getViabilityAndMappingData
   };
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = 3001
 
 const drug_model = require('./drugFetch')
 
+const fs = require("fs");
+
 app.use(express.json())
+
+app.use(cors());
+
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
@@ -22,6 +29,37 @@ app.get('/getDrugs', (req, res) => {
         console.log(error)
         res.status(500).send(error);
     })
+})
+
+app.get('/getDrugData', (req, res) => {
+  drug_model.getDrugData()
+  .then(response => {
+      res.status(200).send(response);
+  })
+  .catch(error => {
+      console.log(error)
+      res.status(500).send(error);
+  })
+})
+
+app.get('/getViabilityAndMappingData', (req, res) => {
+  drug_model.getViabilityAndMappingData()
+  .then(response => {
+    const textData = JSON.stringify(response, null, 2);
+    // fs.writeFile('res.json', textData, (err) => {
+    //     if (err) {
+    //         console.error('Error writing file', err);
+    //         res.status(500).send('Error writing file');
+    //         return;
+    //     }
+    //     console.log('File has been saved');
+    // });
+    res.status(200).send(response);
+  })
+  .catch(error => {
+      console.log(error)
+      res.status(500).send(error);
+  })
 })
 
 app.get('/', (req, res) => {
